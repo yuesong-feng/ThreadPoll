dbb982db5d7afb6c60ddea2c1c6c42b17d5b9153
冯岳松 <yuesong-feng@foxmail.com>
2021-07-25 12:31:52 +0800

    Update ThreadPoll.h

--- ThreadPoll.h
+++ ThreadPoll.h
@@ -1,38 +1,51 @@
-#include <vector>
-#include <thread>
 #include <functional>
-
-class ThreadPoll
-{
-private:
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+class ThreadPoll {
+   private:
     std::vector<std::thread> threads;
-public:
-    ThreadPoll();
+    std::queue<std::function<void()>> tasks;
+    std::mutex tasks_mtx;
+    bool stop;
+
+   public:
+    ThreadPoll(int size);
     ~ThreadPoll();
     template <typename T, typename... Args>
-    void add(std::function<T(Args...)>, Args...);
-    void join_all();
+    void add(std::function<T(Args...)> func, Args... args);
 };
-ThreadPoll::ThreadPoll()
-{
-    std::vector<std::thread> threads;
-}
-ThreadPoll::~ThreadPoll()
-{
-    join_all();
+
+ThreadPoll::ThreadPoll(int size) : stop(false) {
+    for (int i = 0; i < size; ++i) {
+        threads.emplace_back(std::thread([this]() {
+            while (true) {
+                std::function<void()> func;
+                std::unique_lock<std::mutex> tasks_lock(tasks_mtx);
+                if (!tasks.empty()) {
+                    func = tasks.front();
+                    tasks.pop();
+                    tasks_lock.unlock();
+                    func();
+                } else {
+                    tasks_lock.unlock();
+                }
+                if (stop && tasks.empty()) return;
+            }
+        }));
+    }
 }
 
 template <typename T, typename... Args>
-void ThreadPoll::add(std::function<T(Args...)> func, Args... args)
-{
-    threads.push_back(std::thread(func, args...));
+void ThreadPoll::add(std::function<T(Args...)> func, Args... args) {
+    std::function<void()> task = std::bind(func, args...);
+    tasks.push(task);
 }
 
-void ThreadPoll::join_all()
-{
-    for (std::thread &th : threads)
-    {
-        if (th.joinable())
-            th.join();
+ThreadPoll::~ThreadPoll() {
+    stop = true;
+    for (std::thread &th : threads) {
+        if (th.joinable()) th.join();
     }
 }
