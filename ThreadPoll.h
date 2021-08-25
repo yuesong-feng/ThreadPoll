302bf29b7dd5dffa5762c2d0c11133784904dcd1
冯岳松 <yuesong-feng@foxmail.com>
2021-08-25 19:34:21 +0800

    Update ThreadPoll.h

--- ThreadPoll.h
+++ ThreadPoll.h
@@ -1,51 +1,77 @@
+#ifndef THREADPOLL_H
+#define THREADPOLL_H
+
+#include <vector>
+#include <thread>
+#include <queue>
 #include <functional>
 #include <mutex>
-#include <queue>
-#include <thread>
-#include <vector>
-class ThreadPoll {
-   private:
+#include <future>
+#include <condition_variable>
+#include <memory>
+#include <stdexcept>
+
+class ThreadPoll{
+private:
     std::vector<std::thread> threads;
     std::queue<std::function<void()>> tasks;
     std::mutex tasks_mtx;
+    std::condition_variable condition;
     bool stop;
-
-   public:
+public:
     ThreadPoll(int size);
     ~ThreadPoll();
-    template <typename T, typename... Args>
-    void add(std::function<T(Args...)> func, Args... args);
+    template<typename Func, typename... Args>
+    auto add(Func&& func, Args&&... args) 
+        -> std::future<typename std::result_of<Func(Args...)>::type>;
 };
 
-ThreadPoll::ThreadPoll(int size) : stop(false) {
-    for (int i = 0; i < size; ++i) {
-        threads.emplace_back(std::thread([this]() {
-            while (true) {
-                std::function<void()> func;
-                std::unique_lock<std::mutex> tasks_lock(tasks_mtx);
-                if (!tasks.empty()) {
-                    func = tasks.front();
+ThreadPoll::ThreadPoll(int size) : stop(false){
+    for(int i = 0; i < size; ++i){
+        threads.emplace_back([this]{
+            for(;;){
+                std::function<void()> task;
+                {
+                    std::unique_lock<std::mutex> lock(tasks_mtx);
+                    condition.wait(lock, [this]{
+                        return stop || !tasks.empty();
+                    });
+                    if(stop && tasks.empty()) return;
+                    task = std::move(tasks.front());
                     tasks.pop();
-                    tasks_lock.unlock();
-                    func();
-                } else {
-                    tasks_lock.unlock();
                 }
-                if (stop && tasks.empty()) return;
+                task();
             }
-        }));
+        });
     }
 }
 
-template <typename T, typename... Args>
-void ThreadPoll::add(std::function<T(Args...)> func, Args... args) {
-    std::function<void()> task = std::bind(func, args...);
-    tasks.push(task);
+template<typename Func, typename... Args>
+auto ThreadPoll::add(Func&& f, Args&&... args)-> std::future<typename std::result_of<Func(Args...)>::type>{
+    using return_type = typename std::result_of<F(Args...)>::type;
+    auto task = std::make_shared<std::packaged_task<return_type()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    );
+    std::future<return_type> res = task->get_future();
+    {
+        std::unique_lock<std::mutex lock(tasks_mtx);
+        if(stop)
+            throw std::runtime_error("ThreadPoll is already stop, don't add task any more!");
+        tasks.emplace([task](){(*task)();});
+    }
+    condition.notify_one();
+    return res;
 }
 
-ThreadPoll::~ThreadPoll() {
-    stop = true;
-    for (std::thread &th : threads) {
-        if (th.joinable()) th.join();
+ThreadPoll::~ThreadPoll(){
+    {
+        std::unique_lock<std::mutex> lock(tasks_mtx);
+        stop = true;
     }
+    condition.notify_all();
+    for(std::thread &thread : threads)
+        thread.join();
 }
+
+
+#endif
